025bfbb68d38bd070e48864f637820e102ccdf36
Максим Бойчук <maksimbojcuk@Maksims-MacBook-Air.local>
2023-12-25 21:37:16 +0300

    iter1 try1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,93 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strings"
+)
+
+type gauge float64
+type counter int64
+
+type MemStorage struct {
+	gauge   gauge
+	counter counter
+}
+
+type storage interface {
+	replace(new gauge)
+	add(new counter)
+}
+
+func (ms MemStorage) replace(new gauge) {
+	ms.gauge = new
+}
+
+func (ms MemStorage) add(new counter) {
+	ms.counter += new
+}
+
+var memStorage storage
+
+func main() {
+	memStorage = MemStorage{}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", undefinedMetric)
+	mux.Handle("/update/gauge/", middleware(http.HandlerFunc(receiveGaugeMetric)))
+	mux.Handle("/update/counter/", middleware(http.HandlerFunc(receiveCounterMetric)))
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func undefinedMetric(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func receiveGaugeMetric(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	metricData := strings.Split(path, "/")
+	//w.Write([]byte(strings.Join(metricData, "-")))
+	//w.Write([]byte(strconv.Itoa(len(metricData))))
+	if len(metricData) != 5 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if v, ok := interface{}(metricData[4]).(gauge); ok {
+		memStorage.replace(v)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hereOK\n"))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("hereBad\n"))
+	}
+}
+
+func receiveCounterMetric(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	metricData := strings.Split(path, "/")
+	if len(metricData) != 5 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if v, ok := interface{}(metricData[4]).(counter); ok {
+		memStorage.add(v)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MaxBoych/MetricsService
+
+go 1.21.5
