8ef92eadeacc79c66e4ae3d860fb0ad43a4c0ef8
Максим Бойчук <maksimbojcuk@Maksims-MacBook-Air.local>
2024-01-01 15:09:17 +0300

    iter2 try2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,80 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	. "github.com/MaxBoych/MetricsService/cmd/storage"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+const pollInterval = 2
+const reportInterval = 10
+
+var ms MemStorage
+
+func main() {
+	go updateMetrics()
+	go sendMetrics()
+
+	select {}
+}
+
+func updateMetrics() {
+	var stats runtime.MemStats
+	ms = MemStorage{}
+	ms.Gauges = map[string]Gauge{}
+	for {
+		time.Sleep(pollInterval * time.Second)
+
+		runtime.ReadMemStats(&stats)
+
+		ms.Gauges["Alloc"] = Gauge(stats.Alloc)
+		ms.Gauges["BuckHashSys"] = Gauge(stats.BuckHashSys)
+		ms.Gauges["Frees"] = Gauge(stats.Frees)
+		ms.Gauges["GCCPUFraction"] = Gauge(stats.GCCPUFraction)
+		ms.Gauges["GCSys"] = Gauge(stats.GCSys)
+		ms.Gauges["HeapAlloc"] = Gauge(stats.HeapAlloc)
+		ms.Gauges["HeapIdle"] = Gauge(stats.HeapIdle)
+		ms.Gauges["HeapInuse"] = Gauge(stats.HeapInuse)
+		ms.Gauges["HeapObjects"] = Gauge(stats.HeapObjects)
+		ms.Gauges["HeapReleased"] = Gauge(stats.HeapReleased)
+		ms.Gauges["HeapSys"] = Gauge(stats.HeapSys)
+		ms.Gauges["LastGC"] = Gauge(stats.LastGC)
+		ms.Gauges["NextGC"] = Gauge(stats.NextGC)
+		ms.Gauges["NumForcedGC"] = Gauge(stats.NumForcedGC)
+		ms.Gauges["NumGC"] = Gauge(stats.NumGC)
+		ms.Gauges["PauseTotalNs"] = Gauge(stats.PauseTotalNs)
+		ms.Gauges["Mallocs"] = Gauge(stats.Mallocs)
+		ms.Gauges["TotalAlloc"] = Gauge(stats.TotalAlloc)
+		ms.Gauges["OtherSys"] = Gauge(stats.OtherSys)
+		ms.Gauges["Sys"] = Gauge(stats.Sys)
+		ms.Gauges["MCacheInuse"] = Gauge(stats.MCacheInuse)
+		ms.Gauges["MCacheSys"] = Gauge(stats.MCacheSys)
+		ms.Gauges["MSpanInuse"] = Gauge(stats.MSpanInuse)
+		ms.Gauges["MSpanSys"] = Gauge(stats.MSpanSys)
+		ms.Gauges["StackInuse"] = Gauge(stats.StackInuse)
+		ms.Gauges["StackSys"] = Gauge(stats.StackSys)
+		ms.Gauges["Lookups"] = Gauge(stats.Lookups)
+		ms.Gauges["RandomValue"] = Gauge(rand.Float64())
+	}
+}
+
+func sendMetrics() {
+	for {
+		time.Sleep(reportInterval * time.Second)
+
+		for key, value := range ms.Gauges {
+			url := fmt.Sprintf("http://localhost:8080/update/gauge/%s/%s", key, fmt.Sprint(value))
+			response, err := http.Post(url, "text/plain", nil)
+			if err != nil {
+				panic(err)
+			}
+			err = response.Body.Close()
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,98 +1,24 @@
 package main
 
 import (
+	. "github.com/MaxBoych/MetricsService/cmd/handlers"
 	"net/http"
-	"strconv"
-	"strings"
+
+	. "github.com/MaxBoych/MetricsService/cmd/storage"
 )
 
-type gauge float64
-type counter int64
-
-type MemStorage struct {
-	gauge   gauge
-	counter counter
-}
-
-type storage interface {
-	replace(new gauge)
-	add(new counter)
-}
-
-func (ms *MemStorage) replace(new gauge) {
-	ms.gauge = new
-}
-
-func (ms *MemStorage) add(new counter) {
-	ms.counter += new
-}
-
-var memStorage storage
-
 func main() {
-	memStorage = &MemStorage{}
+	ms := &MemStorage{}
+	ms.Init()
+	msHandler := &MetricsHandler{MS: ms}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", undefinedMetric)
-	mux.Handle("/update/gauge/", middleware(http.HandlerFunc(receiveGaugeMetric)))
-	mux.Handle("/update/counter/", middleware(http.HandlerFunc(receiveCounterMetric)))
+	mux.HandleFunc("/", UndefinedMetric)
+	mux.Handle("/update/gauge/", Middleware(http.HandlerFunc(msHandler.ReceiveMetric)))
+	//mux.Handle("/update/counter/", middleware(http.HandlerFunc(receiveCounterMetric)))
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
-	}
-}
-
-func undefinedMetric(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusBadRequest)
-}
-
-func middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/plain")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func receiveGaugeMetric(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	metricData := strings.Split(path, "/")
-	//w.Write([]byte(strings.Join(metricData, "-")))
-	//w.Write([]byte(strconv.Itoa(len(metricData))))
-	if len(metricData) != 5 {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	if value, err := strconv.ParseFloat(metricData[4], 64); err == nil {
-		memStorage.replace(gauge(value))
-		w.WriteHeader(http.StatusOK)
-		//w.Write([]byte("hereOK\n"))
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		//w.Write([]byte("hereBad\n"))
-	}
-}
-
-func receiveCounterMetric(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	metricData := strings.Split(path, "/")
-	//w.Write([]byte(strings.Join(metricData, "-")))
-	//w.Write([]byte(strconv.Itoa(len(metricData))))
-	if len(metricData) != 5 {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	if value, err := strconv.ParseInt(metricData[4], 10, 64); err == nil {
-		memStorage.add(counter(value))
-		w.WriteHeader(http.StatusOK)
-		//w.Write([]byte("hereOK\n"))
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		//w.Write([]byte("hereBad\n"))
 	}
 }
