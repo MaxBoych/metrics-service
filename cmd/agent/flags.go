81552ae94735322d48b4e11eaf4ef2c5a3baac5a
Максим Бойчук <maksimbojcuk@Maksims-MacBook-Air.local>
2024-01-09 22:08:46 +0300

    added env vars

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,6 +1,10 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
 
 var flagRunAddr string
 var flagReportInterval int
@@ -11,4 +15,21 @@ func parseFlags() {
 	flag.IntVar(&flagReportInterval, "r", 10, "frequency of sending metrics on the server")
 	flag.IntVar(&flagPollInterval, "p", 2, "frequency of polling metrics from the 'runtime' package")
 	flag.Parse()
+
+	var err error
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		flagReportInterval, err = strconv.Atoi(envReportInterval)
+		if err != nil {
+			panic(err)
+		}
+	}
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		flagPollInterval, err = strconv.Atoi(envPollInterval)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
